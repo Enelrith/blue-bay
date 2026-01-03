2d4e529aa897aba84a1a278073f5ccf54270b4e8
Enelrith <dimarelefkada@gmail.com>
2026-01-03 19:04:11 +0200

    fix: change createdAt and expiresAt data type to Instant (was LocalDateTime)

--- src/main/java/io/github/enelrith/bluebay/security/entities/RefreshToken.java
+++ src/main/java/io/github/enelrith/bluebay/security/entities/RefreshToken.java
@@ -2,13 +2,21 @@ package io.github.enelrith.bluebay.security.entities;
 
 import io.github.enelrith.bluebay.users.entities.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 
 /**
  * Entity for the "refresh_tokens" table in the database
  */
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "refresh_tokens")
 public class RefreshToken {
     @Id
@@ -19,10 +27,10 @@ public class RefreshToken {
     private String token;
 
     @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 
     @Column(name = "expires_at", nullable = false)
-    private LocalDateTime expiresAt;
+    private Instant expiresAt;
 
     @Column(name = "is_revoked", nullable = false)
     private boolean isRevoked;
