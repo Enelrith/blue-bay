b9b6ae4983ff7bf54303f7793fc6c98840b904e4
Enelrith <dimarelefkada@gmail.com>
2026-01-07 15:11:42 +0200

    feat: add booking_fees table

---
+++ src/main/resources/db/migration/V8__add_booking_fees.sql
@@ -0,0 +1,13 @@
+create table booking_fees(
+    id bigint auto_increment primary key,
+    type varchar(50) not null,
+    amount decimal(10, 3) not null,
+    description varchar(255) not null,
+    constraint ck_amount_positive check (amount > 0)
+);
+
+alter table bookings
+    add column total_climate_fee decimal(10, 2) not null;
+
+alter table bookings
+    add constraint ck_total_climate_fee_positive check (total_climate_fee > 0)
