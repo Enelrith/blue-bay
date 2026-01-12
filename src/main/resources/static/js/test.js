490bb9a489a362b3502ed9ff6972560ffb75f120
Enelrith <dimarelefkada@gmail.com>
2026-01-12 13:41:39 +0200

    feat: add login, get properties and refresh tokens logic for testing

--- src/main/resources/static/js/test.js
+++ src/main/resources/static/js/test.js
@@ -1,10 +1,12 @@
+const baseUrl = "http://localhost:8080";
+
 async function registerUser(event) {
     console.log("clicked")
     event.preventDefault();
     try {
         const email = document.getElementById("register-email").value;
         const password = document.getElementById("register-password").value;
-        const response = await(fetch("http://localhost:8080/users/register", {
+        const response = await(fetch(baseUrl + "/users/register", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -28,3 +30,79 @@ async function registerUser(event) {
 }
 
 document.getElementById("register-user").addEventListener("click", registerUser);
+
+async function loginUser(event) {
+    event.preventDefault();
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
+    try {
+        const response = await(fetch(baseUrl + "/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        }));
+        if (response.ok) {
+            const data = await response.json();
+            const accessToken = data.accessToken;
+            const refreshToken = data.refreshToken;
+            localStorage.setItem("refreshToken", refreshToken)
+            localStorage.setItem("accessToken", accessToken);
+            console.log(data);
+        } else {
+            const error = await response.json();
+            alert(error.message);
+        }
+    } catch(exception) {
+        console.log(exception)
+    }
+}
+
+document.getElementById("login-user").addEventListener("click", loginUser);
+
+async function getProperties(event) {
+    event.preventDefault();
+
+    const response = await(fetch(baseUrl + "/properties", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + localStorage.getItem("accessToken")
+        }
+    }));
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+    } else {
+        const error = await response.json();
+        alert(error.message);
+    }
+}
+
+document.getElementById("get-properties").addEventListener("click", getProperties)
+
+async function refreshToken() {
+    const response = await(fetch(baseUrl + "/auth/refresh", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            refreshToken: localStorage.getItem("refreshToken")
+        })
+    }));
+    if (response.ok) {
+        const data = await response.json();
+        const token = data.accessToken;
+        console.log(data);
+        localStorage.setItem("accessToken", token)
+    } else {
+        const error = await response.json();
+        alert(error.message);
+    }
+}
+document.getElementById("refresh-token").addEventListener("click", refreshToken);
