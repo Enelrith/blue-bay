ae9e49a578b69a56cf34e75a435784af2e080013
Enelrith <dimarelefkada@gmail.com>
2026-01-14 13:48:58 +0200

    docs: add javadocs and swagger documentation to the amenities controller and dto

--- src/main/java/io/github/enelrith/bluebay/amenities/controllers/AmenityController.java
+++ src/main/java/io/github/enelrith/bluebay/amenities/controllers/AmenityController.java
@@ -2,6 +2,9 @@ package io.github.enelrith.bluebay.amenities.controllers;
 
 import io.github.enelrith.bluebay.amenities.dto.AddAmenityRequest;
 import io.github.enelrith.bluebay.amenities.services.AmenityService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +13,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller for the /amenities endpoint
+ */
 @RestController
 @AllArgsConstructor
 @RequestMapping("/amenities")
+@Tag(name = "Amenities")
 public class AmenityController {
     private final AmenityService amenityService;
 
+    /**
+     * Adds an amenity to the database
+     * @param request The new amenity's name
+     * @return 201 Created
+     */
+    @Operation(summary = "Add a new amenity")
+    @ApiResponse(responseCode = "201")
+    @ApiResponse(responseCode = "409", description = "If an amenity with the same name already exists")
+    @ApiResponse(responseCode = "400", description = "If the request includes invalid data")
     @PostMapping
     public ResponseEntity<Void> addAmenity(@RequestBody AddAmenityRequest request) {
         amenityService.addAmenity(request);
--- src/main/java/io/github/enelrith/bluebay/amenities/dto/AddAmenityRequest.java
+++ src/main/java/io/github/enelrith/bluebay/amenities/dto/AddAmenityRequest.java
@@ -1,11 +1,24 @@
 package io.github.enelrith.bluebay.amenities.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 /**
  * DTO for {@link io.github.enelrith.bluebay.amenities.entities.Amenity}
+ * Used in {@link io.github.enelrith.bluebay.amenities.services.AmenityService}
  */
+@Schema(
+        description = "Data required to create a new amenity"
+)
 public record AddAmenityRequest(
-        @Size(message = "Name cannot be more than 100 characters long", max = 100) @NotBlank(message = "Name cannot be blank") String name){
+        @Schema(
+                description = "The name of the amenity",
+                examples = {"Wi-Fi", "Bed", "TV"},
+                maxLength = 100,
+                requiredMode = Schema.RequiredMode.REQUIRED
+        )
+        @Size(message = "Name must be between {min} and {max} characters long", min = 1, max = 100)
+        @NotBlank(message = "Name cannot be blank")
+        String name){
 }
