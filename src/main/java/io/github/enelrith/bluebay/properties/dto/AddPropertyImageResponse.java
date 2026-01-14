f505b938fa2a93d97b35a096dc861ad95c3503bb
Enelrith <dimarelefkada@gmail.com>
2026-01-14 17:15:24 +0200

    refactor: remove redundant image response dto

--- src/main/java/io/github/enelrith/bluebay/properties/dto/AddPropertyImageResponse.java
+++
@@ -1,7 +0,0 @@
-package io.github.enelrith.bluebay.properties.dto;
-
-/**
- * DTO for {@link io.github.enelrith.bluebay.properties.entities.PropertyImage}
- */
-public record AddPropertyImageResponse(String name){
-}
